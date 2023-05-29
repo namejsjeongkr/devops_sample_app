c56b6b888b37dc7caef17337e62c62c0d60eef4a
JinSoo Jeong <121528872+namejsjeongkr@users.noreply.github.com>
2023-05-30 08:35:53 +0900

    Update buildscript.sh
    Delete latest tag

--- .github/buildscript.sh
+++ .github/buildscript.sh
@@ -5,8 +5,6 @@ echo $2
 SERVICE=( "sample-app" )
 for SERVICE in ${SERVICE[@]}; do
     docker tag ${SERVICE}:latest $1/${SERVICE}:$2
-    docker tag ${SERVICE}:latest $1/${SERVICE}:latest
     docker images
     docker push $1/${SERVICE}:$2
-    docker push $1/${SERVICE}:latest
 done
